789d4798f75a2218524511ab68f3f22d81f13c7b
Pavel Alexeev <Pahan@Hubbitus.info>
2008-12-05 12:04:46 +0000

    Update chackYN with support NON-return mode
    git-svn-id: svn+ssh://x-www.info/svn/scripts/trunk@3 11505bf1-1845-4aff-8d8c-c8b78e2d6bc3

--- SHARED/checkYN.bash
+++ SHARED/checkYN.bash
@@ -5,9 +5,17 @@ function checkYN(){
 # $2 - Default value (How threat empty value: 1 - Yes, 0 - No, other - nothing threat).
 # Return 1 in case of answer "yes", 0 - otherwise
 
-local default=''
-local RES=-1
+local default=-1
 local ANS=''
+local non_return_mode=0
+
+	#In "set -e" mode, retun non-0 value stop batch. It is not possible in this case
+	#In additional in this mode we can't use grep because it return 1 if not found pattern
+	if [ '' == "${-##*e*}" ]; then
+	echo 'Warning: Non-return mode. Result *only* in $checkYN_result'
+	checkYN_result=-1
+	non_return_mode=1
+	fi
 
 	if [ ${2:--1} -eq 1 ]; then
 	echo "$1 (Y/n)"
@@ -24,22 +32,26 @@ local ANS=''
 read ANS
 ANS=$( echo $ANS | tr 'A-Z' 'a-z' )
 
-	if [[ ( '' == "$ANS" && '' == $default ) || ( 'y' != "$ANS" && 'n' != "$ANS" && '' != "$ANS" ) ]]; then
+#echo ANS=$ANS
+#echo default=$default
+
+	if [[ ( '' == "$ANS" && -1 -eq $default ) || ( 'y' != "$ANS" && 'n' != "$ANS" && '' != "$ANS" ) ]]; then
 	checkYN "$1" "$2" #recursive call
+	#checkYN_result=$?
+	[ 1 == $non_return_mode ] || return $checkYN_result
 	exit
 	fi
 
-	if [[ 'y' == "$ANS" || 1 -eq $default ]]; then
-	RES=1
+	if [[ 'y' == "$ANS" || ( '' == "$ANS" && 1 -eq $default ) ]]; then
+	checkYN_result=1
 	else
-	RES=0
+	checkYN_result=0
 	fi
-echo $RES;
-return $RES
+
+[ 1 == $non_return_mode ] || return $checkYN_result
 }
 
 #Examples:
-
 #checkYN "Do you want money?" 1
 #checkYN "Do you want money?" 0
 #checkYN "Do you want money?"
