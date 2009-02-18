a23b7083ee09c52d9a85f4744e44c33e859474ce
Pavel Alexeev <Pahan@Hubbitus.info>
2009-02-18 08:55:47 +0000

    Add cleanup old sockets
    git-svn-id: svn+ssh://x-www.info/svn/scripts/trunk@26 11505bf1-1845-4aff-8d8c-c8b78e2d6bc3

--- ssh-agent-reENV.bash
+++ ssh-agent-reENV.bash
@@ -13,9 +13,29 @@
 	exit
 	fi
 
+SSH_AUTH_SOCK=''
+
 #SSH_AUTH_SOCK=$( ls /tmp/ssh-*/agent.* 2>/dev/null )
-SSH_AUTH_SOCK=$( find /tmp -wholename '/tmp/ssh-*/agent.*' -user `id -u` 2>/dev/null )
+#SSH_AUTH_SOCK=$( find /tmp -wholename '/tmp/ssh-*/agent.*' -user `id -u` 2>/dev/null )
+find /tmp -wholename '/tmp/ssh-*/agent.*' -user `id -u` 2>/dev/null | (	# About "(" Read http://bappoy.pp.ru/2008/12/18/bash-pitfalls-part02.html item #7 why we need it!!!
+															# We need set SSH_AUTH_SOCK in while-cycle.
+	while read socket ; do # Check all, cleanup dead
+	agent_pid=$[ ${socket##*.} + 1 ] # I don't known why +1 needed!
+	#D echo socket=$socket
+	#D echo agent_pid=$agent_pid #Agent PID
+		#Check PID ( http://linsovet.com/check-pid-with-kill )
+		if ! kill -0 $agent_pid 2>/dev/null || [ $( ps -p $agent_pid -o comm= ) != "ssh-agent" ] ; then
+		#D echo 'Process dead! Cleanup it.'
+		rm "$socket"
+		else
+		#D echo 'Finded'
+		SSH_AUTH_SOCK="$socket"
+		break; # First occurancies
+		fi
+	done
+
 	if [[ ! "$SSH_AUTH_SOCK" && ${SSH_AGENT_REUSE=true} != false ]]; then
+	#D echo 'Start new ssh-agent'
 	eval `/usr/bin/ssh-agent -s | grep SSH_AUTH_SOCK`
 	fi
 
@@ -24,3 +44,4 @@ SSH_AUTH_SOCK=$( find /tmp -wholename '/tmp/ssh-*/agent.*' -user `id -u` 2>/dev/
 	else
 	export SSH_AUTH_SOCK
 	fi
+)
