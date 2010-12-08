15ffd75583bd2020e6212feca83aa735856d2fa1
Pavel Alexeev <Pahan@Hubbitus.info>
2010-12-08 09:43:59 +0000

    Add second optional parameter - additional error message text
    git-svn-id: svn+ssh://x-www.info/svn/scripts/trunk@128 11505bf1-1845-4aff-8d8c-c8b78e2d6bc3

--- SHARED/assert.bash
+++ SHARED/assert.bash
@@ -3,13 +3,15 @@
 # Got from: http://tldp.org/LDP/abs/html/debugging.html
 
 #######################################################################
+# $1 - assertion to test
+# $2 - (optional) - text of error to add in output if assertion failed
 assert ()                 #  If condition false,
 {                         #+ exit from script
                           #+ with appropriate error message.
 E_ASSERT_FAILED=99
 
 	if [ ! $1 ]; then
-	echo "Assertion failed:  \"$1\""
+	echo "Assertion failed:  \"$1\". $2"
 	echo "Caller (line, file): $( caller )"
 	exit $E_ASSERT_FAILED
 	fi
