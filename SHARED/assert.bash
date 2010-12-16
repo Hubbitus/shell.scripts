b7925f901fd0984a12ac8e98a49c8a830cda8383
Pavel Alexeev <Pahan@Hubbitus.info>
2010-12-16 19:10:05 +0000

    http://hosting-help.ru/blog/30-bash-pipes-and-exit-problem
    git-svn-id: svn+ssh://x-www.info/svn/scripts/trunk@129 11505bf1-1845-4aff-8d8c-c8b78e2d6bc3

--- SHARED/assert.bash
+++ SHARED/assert.bash
@@ -17,3 +17,30 @@ E_ASSERT_FAILED=99
 	fi
 }
 #######################################################################
+
+
+# When command executed in pipe it acts in subshell according to bash man
+# (and http://webcache.googleusercontent.com/search?q=cache:MgGItz54hdgJ:steve-parker.org/sh/functions.shtml+bash+exit+in+piped+function&cd=4&hl=ru&ct=clnk&gl=ru )
+# There no standard way to terminate parent (main script) in this case regard of used "return" or "exit" statements in function:
+# http://stackoverflow.com/questions/4419952/difference-between-return-and-exit-in-bash-functions , http://stackoverflow.com/questions/1816824/exit-entire-program-from-a-function-call-in-shell .
+# Exception from that I found solution kill himself like "kill $$" - http://www.unix.com/shell-programming-scripting/138182-functions-exit-kill-bash.html
+# It may work in this particular solution, but also have some downsides like unavailable pass status code, may kill not intended script when it sourced and so on.
+# Another way do that job is explicitly check $PIPESTATUS special variable (More examples and detailes: http://unix.derkeiler.com/Newsgroups/comp.unix.shell/2005-06/1079.html )
+# So, to provide also exit code from assert I wrote this small helper function.
+#
+# Example of usage:
+# assert '1 -gt 2' | tee LOG
+# pipe_check_exit ${PIPESTATUS: -1}
+#
+# $1 - pipestatus code
+function pipe_check_exit(){
+#	[ ${PIPESTATUS: -1} -gt 0 ] && exit ${PIPESTATUS: -1}
+	[ $1 -gt 0 ] && exit $1
+}
+#/f pipe_check_exit
+
+# $1 = message to log
+function log(){
+	echo "$1" | tee -a "$LOG"
+}
+#/f log
