6839cdc59e2c0cb1d4391ff6c1507350206702ec
Pavel Alexeev <Pahan@Hubbitus.info>
2011-09-30 08:38:51 +0000

    Make not_enouth_argument more clear
    git-svn-id: svn+ssh://x-www.info/svn/scripts/trunk@137 11505bf1-1845-4aff-8d8c-c8b78e2d6bc3

--- SHARED/not_enough_args.bash
+++ SHARED/not_enough_args.bash
@@ -3,3 +3,5 @@
 #Должен быть хотя бы 1 аргуемнт
 #http://www.opennet.ru/docs/RUS/bash_scripting_guide/x7794.html#DERPM
 : ${1?"Порядок использования: `basename $0` target-file"}
+
+: ${1?"Not enough arguments: `basename $0` target-file"}
