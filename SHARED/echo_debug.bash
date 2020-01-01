f0ab87047081e18ee5250e523f6b263741b01b28
Pavel Alexeev <pahan@hubbitus.info>
2020-01-01 16:34:48 +0300

    ECHO_DEBUG_file by default /dev/stderr

--- SHARED/echo_debug.bash
+++ SHARED/echo_debug.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #D ECHO_DEBUG=true
-ECHO_DEBUG_file=${ECHO_DEBUG_file:-/dev/stdout}
+ECHO_DEBUG_file=${ECHO_DEBUG_file:-/dev/stderr}
 
 function echo_debug(){
 	[ "$ECHO_DEBUG" ] && echo "$@" >> "$ECHO_DEBUG_file"
