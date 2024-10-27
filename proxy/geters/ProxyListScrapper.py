3145262eb2713efbe8b9f09d31ca8db8ca354f36
Pavel Alexeev <plalexeev@gid.ru>
2024-10-27 21:19:41 +0300

    Add 13_freeproxyupdate.com.get and implement pultithreading

--- proxy/geters/ProxyListScrapper.py
+++ proxy/geters/ProxyListScrapper.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup, Tag, PageElement
 
 
 class Proxy:
@@ -33,23 +33,45 @@ class ProxyListScrapper:
         self.basedir = pathlib.Path(basedir)
         self.urls = urls
 
-        self.parse()
+        self.run()
+
+    def run(self):
+        self.parse_pages(self.urls)
+        self.finalize()
 
     def filename(self, suffix: str) -> str:
         return os.path.join(self.basedir, f'{self.name}.{suffix}.txt')
 
-    def parse(self) -> None:
+
+    def parse_pages(self, urls: dict[str,str]) -> None:
         """
         Main method to start parsing
         :return:
         """
-        for file_type, url in self.urls.items():
-            soup = self._download_page(url)
-            proxy_list: ProxyList = self.get_proxy_list(soup)
+        # for idx,(file_type, url) in enumerate(urls.items()):
+        #     print(f'{idx}/{len(urls)-1}) Processing page parsing {file_type} from {url}')
+        #     self.process_page(file_type, url)
 
-            with open(self.filename(file_type), 'w') as file:
-                for proxy in proxy_list:
-                    file.write(str(proxy) + '\n')
+
+
+        from multiprocessing.pool import ThreadPool
+
+        lst = [(idx, file_type, url) for idx, (file_type, url) in enumerate(urls.items())]
+
+        with ThreadPool(os.cpu_count() * 5) as pool:
+            list(pool.map_async(lambda tpl: print(f'{tpl[0]}/{len(urls)-1}) Processing page parsing {tpl[1]} from {tpl[2]}') or self.process_page(tpl[1], tpl[2]), lst).get())
+
+
+    def process_page(self, file_type, url) -> str:
+        soup = self._download_page(url)
+        proxy_list: ProxyList = self.get_proxy_list(soup)
+        with open(self.filename(file_type), 'w') as file:
+            for proxy in proxy_list:
+                file.write(str(proxy) + '\n')
+
+        # Recursively parse links if so
+        self.parse_pages(self.get_other_page_links(soup, file_type, url))
+        return url
 
 
     @staticmethod
@@ -72,13 +94,21 @@ class ProxyListScrapper:
         proxy_list = ProxyList()
 
         for tr in self.get_table_trs(soup):
-            if tr.td is None or tr.td.get('colspan', False):  # <thead>
+            if self.is_skip_tr(tr):
                 continue
 
             proxy = self.parse_table_tr_to_proxy(tr)
             proxy_list.add(proxy)
 
         return proxy_list
+
+
+    def get_other_page_links(self, soup: BeautifulSoup, file_type: str, url: str) -> dict[str,str]:
+        """
+        Possibly a useful method to get other links from HTML source. For example, walk by pages if there present a navigation
+        Good candidate for the overriding.
+        """
+        return {}
 
 
     @staticmethod
@@ -100,3 +130,16 @@ class ProxyListScrapper:
         """
         tds = tr.find_all('td')
         return Proxy(tds[0].text, tds[1].text, tds[2].text.lower())
+
+
+    @staticmethod
+    def is_skip_tr(tr: Tag) -> bool:
+        return tr.td is None or tr.td.get('colspan', False)
+
+    def finalize(self):
+        """
+        Method called after all parsing done. For example for temporary files cleanup
+        Good candidate for the overriding.
+        :return:
+        """
+        print('Done')
